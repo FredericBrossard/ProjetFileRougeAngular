70b6a731b7c4813aeb857f253113c447d8a5b601
Emma Contardo <emmanuelle.contardo.pf1@gmail.com>
2018-06-15 11:26:24 +0200

    Creation branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { Page404Component } from './page404/page404.component';
 
 
 
+
 const routes: Routes = [
   { path: '', redirectTo: '/repas', pathMatch: 'full' },
    {
--- src/app/historique/historique.component.ts
+++ src/app/historique/historique.component.ts
@@ -13,3 +13,5 @@ export class HistoriqueComponent implements OnInit {
   }
 
 }
+
+
